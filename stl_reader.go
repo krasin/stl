a32c24e47c4759b024902ccf5562f85088061061
Ivan Krasin <imkrasin@gmail.com>
2012-02-12 12:54:29 -0800

    Add stl_reader and test

---
+++ stl_reader.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"unsafe"
+)
+
+const SizeOfSTLTriangle = 4*3*4 + 2
+
+type STLPoint [3]float32
+
+type STLTriangle struct {
+	n STLPoint
+	v [3]STLPoint
+}
+
+func readSTLPoint(a []byte, p *STLPoint) []byte {
+	for i := 0; i < 3; i++ {
+		cur := uint32(a[0]) + uint32(a[1])<<8 + uint32(a[2])<<16 + uint32(a[3])<<24
+		p[i] = *(*float32)(unsafe.Pointer(&cur))
+		a = a[4:]
+	}
+	return a
+}
+
+func readLineWithPrefix(r *bufio.Reader, prefixes ...string) (prefix, str string, err error) {
+	var line []byte
+	if line, _, err = r.ReadLine(); err != nil {
+		return
+	}
+	str = strings.TrimSpace(string(line))
+	for _, pp := range prefixes {
+		if strings.HasPrefix(str, pp) {
+			return pp, str[len(pp):], nil
+		}
+	}
+	return "", "", fmt.Errorf("Line expected to start with one of the prefixes: %v, the actual line is: '%s'", prefixes, str)
+}
+
+func consumeLine(r *bufio.Reader, want string) (err error) {
+	var str string
+	if _, str, err = readLineWithPrefix(r, want); err != nil {
+		return
+	}
+	if str != "" {
+		return fmt.Errorf("Line contains unexpected symbols after the right prefix: '%s', symbols: '%s'", want, str)
+	}
+	return nil
+}
+
+func readAsciiSTL(data []byte) (res []STLTriangle, err error) {
+	r := bufio.NewReader(bytes.NewBuffer(data))
+	if err = consumeLine(r, "solid object"); err != nil {
+		return
+	}
+	for {
+		var prefix, str string
+		var t STLTriangle
+		if prefix, str, err = readLineWithPrefix(r, "facet normal ", "endsolid object"); err != nil {
+			if err == io.EOF {
+				return res, nil
+			}
+			return nil, err
+		}
+		if prefix == "endsolid object" {
+			return
+		}
+		fields := strings.Fields(str)
+		if len(fields) != 3 {
+			return nil, fmt.Errorf("Normal definition is broken: '%s'", str)
+		}
+		for i := 0; i < 3; i++ {
+			var v float64
+			if v, err = strconv.ParseFloat(fields[i], 32); err != nil {
+				return nil, err
+			}
+			t.n[i] = float32(v)
+		}
+		if err = consumeLine(r, "outer loop"); err != nil {
+			return nil, err
+		}
+		for i := 0; i < 3; i++ {
+			if _, str, err = readLineWithPrefix(r, "vertex "); err != nil {
+				return nil, err
+			}
+			fields = strings.Fields(str)
+			if len(fields) != 3 {
+				return nil, fmt.Errorf("Vertex definition is broken: '%s'", str)
+			}
+			for j := 0; j < 3; j++ {
+				var v float64
+				if v, err = strconv.ParseFloat(fields[j], 32); err != nil {
+					return nil, err
+				}
+				t.v[i][j] = float32(v)
+			}
+		}
+		if err = consumeLine(r, "endloop"); err != nil {
+			return nil, err
+		}
+		if err = consumeLine(r, "endfacet"); err != nil {
+			return nil, err
+		}
+
+		res = append(res, t)
+
+	}
+	return
+}
+
+func ReadSTL(r io.Reader) (t []STLTriangle, err error) {
+	var data []byte
+	if data, err = ioutil.ReadAll(r); err != nil {
+		return
+	}
+	if len(data) < 5 {
+		return nil, fmt.Errorf("The file is too short: %d bytes", len(data))
+	}
+	magic := data[:5]
+	if string(magic) == "solid" {
+		return readAsciiSTL(data)
+	}
+	// Skip STL header
+	data = data[80:]
+	n := uint32(data[0]) + uint32(data[1])<<8 + uint32(data[2])<<16 + uint32(data[3])<<24
+	data = data[4:]
+
+	if len(data) < int(SizeOfSTLTriangle*n) {
+		return nil, fmt.Errorf("ReadSTL: unexpected end of file: want %d bytes to read triangle data, but only %d bytes is available", SizeOfSTLTriangle*n, len(data))
+	}
+	for i := 0; i < int(n); i++ {
+		var cur STLTriangle
+		data = readSTLPoint(data, &cur.n)
+		for j := 0; j < 3; j++ {
+			data = readSTLPoint(data, &cur.v[j])
+		}
+		data = data[2:]
+		t = append(t, cur)
+	}
+	return
+}
---
+++ stl_reader_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"math"
+	"os"
+	"testing"
+)
+
+const eps = 1E-4
+
+type readSTLTest struct {
+	filename string
+	count    int
+	t        []STLTriangle
+}
+
+var readSTLTests = []readSTLTest{
+	{"data/cylinder.bin.stl", 326, nil},
+	{
+		"data/cylinder.stl",
+		326,
+		[]STLTriangle{
+			{
+				[3]float32{0, 0, 0},
+				[3]STLPoint{
+					{-7.708244e-01, -3.846672e+00, 5.378669e+00},
+					{-1.548386e+00, -3.683723e+00, 4.516774e+00},
+					{-1.530743e+00, -3.695526e+00, 4.194193e+00},
+				},
+			},
+		},
+	},
+}
+
+func STLCoordEqual(a, b float32) bool {
+	if math.IsNaN(float64(a)) || math.IsNaN(float64(b)) {
+		return false
+	}
+	return math.Abs(float64(a)-float64(b)) < eps
+}
+
+func STLPointEqual(p1, p2 [3]float32) bool {
+	for i := 0; i < 3; i++ {
+		if !STLCoordEqual(p1[i], p2[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func STLEqual(t1, t2 STLTriangle) bool {
+	return STLPointEqual(t1.n, t2.n) &&
+		STLPointEqual(t1.v[0], t2.v[0]) &&
+		STLPointEqual(t1.v[1], t2.v[1]) &&
+		STLPointEqual(t1.v[2], t2.v[2])
+}
+
+func TestReadSTL(t *testing.T) {
+	for _, test := range readSTLTests {
+		f, err := os.Open(test.filename)
+		if err != nil {
+			t.Fatalf("os.Open(\"%v\"): %v", test.filename, err)
+		}
+		defer f.Close()
+		stl, err := ReadSTL(f)
+		if err != nil {
+			t.Fatalf("ReadSTL: %v", err)
+		}
+		if len(stl) != test.count {
+			t.Fatalf("Wrong number of triangles. Expected: %d, got: %d", test.count, len(stl))
+		}
+		for i, tr := range test.t {
+			if !STLEqual(tr, stl[i]) {
+				t.Fatalf("Triangle #%d, want: %v, got: %v", i, tr, stl[i])
+			}
+		}
+	}
+}
